c7d4249a878bffc8323397859c2c94fadd3b84d8
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-02 01:43:11 +0530

    ShareBox started

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Main = () => {
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+    <Container>
+      <ShareBox>Share</ShareBox>
+    </Container>
   );
 };
 
 export default Main;
+
+const Container = styled.div``;
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.15), 0 0 0 rgba(0, 0, 0, 0.2);
+`;
+
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background-color: white;
+`;
